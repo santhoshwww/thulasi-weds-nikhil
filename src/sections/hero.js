4a3811132b28acfe5862abf59bcc534e4d47691e
santhoshwww <80646328+santhoshwww@users.noreply.github.com>
2026-07-26 05:00:53 +0530

    Create hero.js

---
+++ src/sections/hero.js
@@ -0,0 +1,46 @@
+export function renderHero() {
+  return `
+<section class="hero">
+
+    <div class="hero-overlay"></div>
+
+    <div class="sun"></div>
+
+    <div class="cloud cloud-left"></div>
+    <div class="cloud cloud-right"></div>
+
+    <div class="birds"></div>
+
+    <div class="temple"></div>
+
+    <div class="hero-content">
+
+        <p class="subtitle">
+            Together With Their Families
+        </p>
+
+        <h1>
+            Thulasi
+            <span class="heart">❤</span>
+            Nikhil
+        </h1>
+
+        <h2>
+            Begin Our Forever
+        </h2>
+
+        <div class="scroll-indicator">
+
+            <span></span>
+
+            <p>Scroll</p>
+
+        </div>
+
+    </div>
+
+    <div class="petal-container"></div>
+
+</section>
+`;
+}
