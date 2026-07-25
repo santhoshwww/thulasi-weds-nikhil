aee927c457e9a8d3a8d1dad9bc25394bbb7eadd1
santhoshwww <80646328+santhoshwww@users.noreply.github.com>
2026-07-26 04:33:09 +0530

    Create hero.js

---
+++ hero.js
@@ -0,0 +1,33 @@
+export function hero() {
+
+return `
+
+<section class="hero">
+
+<div class="overlay"></div>
+
+<div class="sun"></div>
+
+<div class="temple"></div>
+
+<div class="content">
+
+<h2>Thulasi ❤️ Nikhil</h2>
+
+<p>Begin Our Forever</p>
+
+<div class="scroll">
+
+↓
+
+</div>
+
+</div>
+
+<div class="petals"></div>
+
+</section>
+
+`;
+
+}
